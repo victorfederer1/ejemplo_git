069427177340254a5cf5b033d5cc5e138bfbf323
victorfederer1 <federer4321@protonmail.com>
2025-10-26 12:13:13 +0100

    Initial commit

---
+++ app.ts
@@ -0,0 +1,16 @@
+console.log('Hello, TypeScript!');
+
+type User = {
+    id: number,
+    name: string,
+    email: string
+}
+
+const user: User = {
+    id: 1,
+    name: 'Alice',
+    email: 'alice@example.com'
+}
+
+
+
